a97873532d527b417521f3a2ac28e5a1914490b2
emilielydiat <emilielydiat@gmail.com>
2025-09-09 18:11:21 +0100

    feat(settings): add MealPriorities page

---
+++ src/pages/MealPriorities.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import { usePageHeader } from "../hooks/usePageHeader";
+
+export function MealPriorities() {
+  usePageHeader("Set meal priorities", true);
+
+  return <Typography>Meal priorities page (coming soon)</Typography>;
+}
